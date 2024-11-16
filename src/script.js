d26e0b335aecb511efd05dc9c78c1f6c6db5bef8
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-11-16 16:46:08 -0300

    Feat: event click, cond

--- src/script.js
+++ src/script.js
@@ -5,3 +5,33 @@ let buttons = document.querySelectorAll("#buttons-container button");
 let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let secondPlayer;
+
+//contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+
+//adicionando  o evento de click aos boxes
+
+for(let i = 0; i < boxes.length; i++) {
+
+    // quando alguem clica na caixa
+    boxes[i].addEventListener("click", function() {
+
+        let el;
+
+        if(player1 == player2) {
+            //x
+            el = x;
+        } else {
+            //o
+            el = o;
+        }
+
+        let cloneEl = el.cloneNode(true)
+
+        this.appendChild(cloneEl);
+
+    })
+
+}
