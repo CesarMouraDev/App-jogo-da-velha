a05755eaa22c129919f05920d37f3a427b1d407f
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-11-16 16:27:09 -0300

    Feat: Script, Create Let

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-console.log("Ola mundo")
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
