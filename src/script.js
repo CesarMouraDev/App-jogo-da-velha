a030bbd7200026ae5717baadc98df28ce2a95dd5
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-11-16 17:09:24 -0300

    Feat: fist cond line victory

--- src/script.js
+++ src/script.js
@@ -18,16 +18,10 @@ for(let i = 0; i < boxes.length; i++) {
     // quando alguem clica na caixa
     boxes[i].addEventListener("click", function() {
 
-        let el;
+        let el = checkEl(player1, player2);
+      
 
-        if(player1 == player2) {
-            //x
-            el = x;
-        } else {
-            //o
-            el = o;
-        }
-
+    //verifica se ja tem x ou o
       if(this.childNodes.length == 0) {
         let cloneEl = el.cloneNode(true)
 
@@ -39,8 +33,51 @@ for(let i = 0; i < boxes.length; i++) {
        } else {
             player2++;
        }
+       //checa quem venceu
+       checkWinCondition();
+
+
       }
 
     })
+}
 
+function checkEl(player1, player2) {
+    if(player1 == player2) {
+        //x
+        el = x;
+    } else {
+        //o
+        el = o;
+    }
+    return el
+}
+
+// ve quem ganhou
+
+function checkWinCondition() {
+    let b1 = document.getElementById("block-1")
+    let b2 = document.getElementById("block-2")
+    let b3 = document.getElementById("block-3")
+    let b4 = document.getElementById("block-4")
+    let b5 = document.getElementById("block-5")
+    let b6 = document.getElementById("block-6")
+    let b7 = document.getElementById("block-7")
+    let b8 = document.getElementById("block-8")
+    let b9 = document.getElementById("block-9")
+
+    // horizontal
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+
+        if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x') {
+            //x
+            console.log("x venceu")
+        } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o') {
+            //o
+          }
+
+    }
 }
